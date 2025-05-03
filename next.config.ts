c5f1b6da7b3139f62d95aeb4502791c80dc92f62
NebiyouChanie <cnebiyu@gmail.com>
2025-05-03 22:15:18 +0300

    upate next config

--- next.config.ts
+++ next.config.ts
@@ -2,14 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "res.cloudinary.com",
-        },
-    ],
+    domains: ["res.cloudinary.com"], 
   },
-  output: "standalone",
 };
 
 export default nextConfig;
